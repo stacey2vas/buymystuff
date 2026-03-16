fda97e041e37104bf9d73f74141c31e9bf03345a
julienM29 <137764146+julienM29@users.noreply.github.com>
2026-03-16 16:11:41 +0100

    Premier controller

--- src/main/java/fr/eni/buymystuff/controllers/EnchereController.java
+++ src/main/java/fr/eni/buymystuff/controllers/EnchereController.java
@@ -1,5 +1,17 @@
 package fr.eni.buymystuff.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EnchereController {
 
+    public EnchereController() {
+    }
+
+    @GetMapping("/")
+    public String accueil(){
+
+        return "accueil";
+    }
 }
