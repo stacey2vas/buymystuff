b5d989148cf70e45e697b8c4d28ffc9d5d2034db
Stacey FERREIRA DE VASCONCELOS <stacey.ferreiradevasconcelos2026@campus-eni.fr>
2026-03-16 12:28:15 +0100

    Création du projet enchère

---
+++ src/main/java/fr/eni/buymystuff/BuymystuffApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.buymystuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuymystuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuymystuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/buymystuff/BuymystuffApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.buymystuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuymystuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
