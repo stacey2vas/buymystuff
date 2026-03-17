d361cd84693a018e2a82fdfa4027b9595adb7ecc
wandev35 <80387034+wandev35@users.noreply.github.com>
2026-03-17 13:44:35 +0100

    début login

--- src/main/java/fr/eni/buymystuff/controllers/AuthTestController.java
+++ src/main/java/fr/eni/buymystuff/controllers/AuthTestController.java
@@ -1,4 +1,28 @@
 package fr.eni.buymystuff.controllers;
 
+import fr.eni.buymystuff.bo.Utilisateurs;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AuthTestController {
+    @GetMapping("/show-auth-form")
+    public String showAuthForm(Model model) {
+        // Instancier un utilisateur par defaut dans le formulaire
+        Utilisateurs utilisateur = new Utilisateurs();
+
+        model.addAttribute("utilisateur", utilisateur);
+
+        return "test/login-auth-page";
+    }
+
+    @PostMapping ("/process-auth")
+    public String processAuth(@ModelAttribute("utilisateur") Utilisateurs utilisateur, Model model) {
+        //Dev en cours
+        //TODO : Gérer le process de connexion.
+        return null;
+    }
 }
