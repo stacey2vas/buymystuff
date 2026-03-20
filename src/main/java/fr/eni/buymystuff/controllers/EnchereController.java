e15c14fb14915ad733d4114ed7db18c6233a8ac7
sferreiradevasc2026 <stacey.ferreiradevasconcelos2026@campus-eni.fr>
2026-03-20 16:36:55 +0100

    creation d'Encheres et test CSS

--- src/main/java/fr/eni/buymystuff/controllers/EnchereController.java
+++ src/main/java/fr/eni/buymystuff/controllers/EnchereController.java
@@ -1,7 +1,17 @@
 package fr.eni.buymystuff.controllers;
 
 
+import fr.eni.buymystuff.bo.Encheres;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class EnchereController {}
+public class EnchereController {
+
+    @GetMapping("/enchere")
+    public String enchere(Model model) {
+        model.addAttribute("enchere", new Encheres());
+        return "test/enchere";
+    }
+}
