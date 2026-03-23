eb86dba67fcd797874b1102613750c04cef6ede1
sferreiradevasc2026 <stacey.ferreiradevasconcelos2026@campus-eni.fr>
2026-03-23 09:36:02 +0100

    ctaegoriesController ajout

---
+++ src/main/java/fr/eni/buymystuff/controllers/CategoriesController.java
@@ -0,0 +1,55 @@
+package fr.eni.buymystuff.controllers;
+
+import fr.eni.buymystuff.bo.Categories;
+import fr.eni.buymystuff.services.ServiceResponse;
+import fr.eni.buymystuff.services.ServicesCategories;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CategoriesController {
+    private final ServicesCategories servicesCategories;
+
+    public CategoriesController(ServicesCategories servicesCategories) {
+        this.servicesCategories = servicesCategories;
+    }
+
+    @GetMapping("/ajout-categories")
+    public String showForm(Model model) {
+        model.addAttribute("categorie", new Categories());
+        return "ajout-categories";
+    }
+
+    @GetMapping("/modifier-categorie/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+
+        ServiceResponse<Categories> response = servicesCategories.selectById(id);
+        if (response.getData() != null) {
+            model.addAttribute("categorie", response.getData());
+        } else {
+            model.addAttribute("message", "La catégorie avec l'ID " + id + " n'existe pas.");
+            model.addAttribute("categorie", new Categories());
+        }
+        return "ajout-categories";
+    }
+
+    @PostMapping("/ajout-categories")
+    public String save(@ModelAttribute Categories categorie, Model model) {
+
+        ServiceResponse<Categories> response = servicesCategories.saveCategories(categorie);
+        if(response.getCode().equals("2005")){
+            model.addAttribute("categorie", categorie);
+            model.addAttribute("message", response.getMessage());
+            return "ajout-categories";
+        }
+        else {
+            model.addAttribute("message", response.getMessage());
+            model.addAttribute("categorie", new Categories());
+            return "ajout-categories";
+        }
+    }
+}
---
+++ src/main/java/fr/eni/buymystuff/dao/DAOEnchere.java
@@ -0,0 +1,4 @@
+package fr.eni.buymystuff.dao;
+
+public class DAOEnchere {
+}
