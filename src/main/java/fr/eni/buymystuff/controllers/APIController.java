e0c93468b1c9d4fa6a3201984bb06116af596ed0
julienM29 <137764146+julienM29@users.noreply.github.com>
2026-03-24 10:16:55 +0100

    Vérifications de l'enchère : OK

--- src/main/java/fr/eni/buymystuff/controllers/APIController.java
+++ src/main/java/fr/eni/buymystuff/controllers/APIController.java
@@ -2,8 +2,14 @@ package fr.eni.buymystuff.controllers;
 
 import java.util.List;
 
+import fr.eni.buymystuff.bo.Utilisateurs;
+import fr.eni.buymystuff.services.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +21,15 @@ import fr.eni.buymystuff.services.ArticleService;
 @Controller
 @RequestMapping("/api")
 public class APIController {
-        private final ArticleService articleService;
-    
-        public APIController(ArticleService articleService) {
-            this.articleService = articleService;
-        }
+    private final ArticleService articleService;
+    private final AuthService authService;
 
-        @PostMapping("/articles")
+    public APIController(ArticleService articleService, AuthService authService) {
+        this.articleService = articleService;
+        this.authService = authService;
+    }
+
+    @PostMapping("/articles")
     public String getArticles(@RequestBody ArticleFilterDTO filter, Model model) {
         try {
             // récupère les articles filtrés
@@ -34,5 +42,15 @@ public class APIController {
             return "fragments/cardArticle :: cardArticle";
         }
     }
-    
+    @GetMapping("/creditUser")
+    public ResponseEntity<Integer> getCreditUser(@AuthenticationPrincipal UserDetails userDetails) {
+        System.out.println("je passe ici");
+        Utilisateurs user = authService.getUserByPseudo(userDetails.getUsername()).data;
+        return ResponseEntity.ok(user.getCredit());
+    // ResponseEntity est une classe de Spring qui te permet de contrôler complètement la réponse HTTP.
+        //👉 Avec ça tu peux gérer :
+        //le body (les données envoyées)
+        //le status HTTP (200, 404, 401…)
+        //les headers
+        }
 }
